7054ac514f80338c8d13985ef279d91c782bb7a9
bryanesteban <bryanesteban_123@hotmail.com>
2024-04-28 00:18:12 -0500

    consult of service

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit{
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
@@ -23,8 +24,24 @@ export class SelectorPageComponent {
     private countriesService: CountriesService
   ){}
 
+  ngOnInit(): void {
+    this.onRegionChange();
+
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  onRegionChange(): void {
+    this.myForm.get('region')!.valueChanges
+    .pipe(
+      switchMap( region => this.countriesService.getCountriesByRegion(region),
+      )
+    )
+    .subscribe(region => {
+      console.log({region});
+    });
   }
 
 }
